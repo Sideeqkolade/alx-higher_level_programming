cda29027e9e15dd0b9a47b4eab2c53d0e31a379f
Sideeqkolade <sideeqkolade@gmail.com>
2023-01-06 01:35:05 +0000

    import modules

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """program that imports all functions from the file and handles basic operations."""
+    from calculator_1.py import add, sub, mul, div
+    import sys
+
+    count = len(sys.argv) - 1
+    if count != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.key()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the result of the addition of all arguments"""
+    import sys
+
+    total = 0
+    count = len(sys.argv) - 1
+    for i in range(count):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
