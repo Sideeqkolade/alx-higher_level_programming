139f63670e6668b05fc4bf97fe2239b56e521896
Sideeqkolade <sideeqkolade@gmail.com>
2023-01-25 16:27:43 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""defines a class square"""
+
+
+class Square:
+    """A square"""
+
+    def __init__(self, size):
+        """Initalizes a new square
+
+
+        Args:
+            size (int): size of square
+        """
+
+        self.__size = size
