13428e53cb92a92f89f6a7d9a232d22fea668412
Sideeqkolade <sideeqkolade@gmail.com>
2023-02-01 13:25:25 +0100

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines the addition of two integers"""
+
+def add_integer(a, b=98):
+    """Returns the addition of a and b
+
+    a and b must be casted into integer if they are of float type
+
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    elif ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    else:
+        return (int (a) + int (b))
