f3200b4d1899b30423fa418755c133f774f9d221
Sideeqkolade <sideeqkolade@gmail.com>
2023-04-11 20:06:52 +0100

    task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
 // script that prints three lines with one command.
+// only console.log is used to print all output.
 
 console.log('C is fun\nPython is cool\nJavaScript is amazing');
 
