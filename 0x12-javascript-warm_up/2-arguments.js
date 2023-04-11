3b8b08e32e443862c8b6e9d02622f55e66c8d43a
Sideeqkolade <sideeqkolade@gmail.com>
2023-04-12 00:12:29 +0100

    task 2 updated

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
-// a script that prints a message depending of the number of arguments passed
-// If no arguments are passed to the script, print “No argument”
-// Otherwise, print “Arguments found”
-// You must use console.log(...) to print all output
-// You are not allowed to use var
+
+//  a script that prints a message
+//  depending of the number of arguments passed
+//  If no arguments are passed to the script, print “No argument”
+//  Otherwise, print “Arguments found”
+//  You must use console.log(...) to print all output
+//  You are not allowed to use var
 
 const arg = process.argv.slice(2);
+
 if (arg.length === 0) {
   console.log('No argument');
 } else if (arg.length === 1) {
@@ -13,4 +16,3 @@ if (arg.length === 0) {
 } else {
   console.log('Arguments found');
 }
-
