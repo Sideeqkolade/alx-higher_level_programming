a16af400bd8368d17aab86d53c1c28dee95a373f
Sideeqkolade <sideeqkolade@gmail.com>
2023-04-15 19:30:35 +0100

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     cur = conn.cursor()
     state = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE name LIKE %s", (state, ))
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY states.id ASC", (state, ))
     rows = cur.fetchall()
     for row in rows:
         print(row)
