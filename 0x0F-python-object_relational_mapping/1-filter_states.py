18674e3d98d310236cd704164c79507e86bc5f7c
Sideeqkolade <sideeqkolade@gmail.com>
2023-04-15 18:29:27 +0100

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a script that lists all states with a name starting with N  the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+            host='localhost',
+            user=sys.argv[1],
+            password=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+            )
+
+    cur = conn.cursor()
+    """this creates the cursor for connection"""
+
+    cur.execute("SELECT * FROM states WHERE name  LIKE BINARY 'N%' ORDER BY states.id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
