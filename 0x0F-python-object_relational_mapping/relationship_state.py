d9d5d5d7c5737aab89a5542e8aa8179a5fa65881
Sideeqkolade <sideeqkolade@gmail.com>
2023-04-17 18:38:50 +0100

    task 100

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Write a python file that contains the class definition \
+of a State and an instance Base = declarative_base()"""
+
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+"""Import sqlalchemy"""
+
+Base = declarative_base()
+"""create a base class for ORM models"""
+
+
+class State(Base):
+    """
+    A class that inherits from base
+    Args:
+        id: auto-generated, unique integer, can't be null
+            primary key
+        name: string with max 128 characters, can't be null
+    """
+
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='all, delete')
