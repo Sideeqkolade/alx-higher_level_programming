4d16e7790eb66f2e6f50ae8a4d6120e5742f2d6e
root <sideeqkolade@gmail.com>
2023-04-29 01:45:15 +0100

    task 9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" a Python script that takes your GitHub credentials (username
+    and password)
+    and uses the GitHub API to display your id
+    You must use Basic Authentication with a personal access token
+    as password to access to your information
+    (only read:user permission is needed)
+    The first argument will be your username
+    The second argument will be your password (in your case,
+    a personal access token as password)
+    You must use the package requests and sys
+    You are not allowed to import packages other than requests and sys
+    You don’t need to check arguments passed to the script (number or type)
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: ./github_id.py <username> <password>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = 'https://api.github.com/user'
+    auth = (username, password)
+
+    response = requests.get(url, auth=auth)
+    if response.status_code == 200:
+        response_json = response.json()
+        user_id = response_json.get("id")
+        print(user_id)
+
+    else:
+        print(f"Error: {response.status_code} {response.reason}")
+        sys.exit(1)
