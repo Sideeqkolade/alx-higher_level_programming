e17f5da82cd4b0f4c6b0731c6594ddc3ec9a2ef6
root <sideeqkolade@gmail.com>
2023-04-29 03:00:35 +0100

    task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" a Python script that takes 2 arguments in order to solve this challenge.
+    The first argument will be the repository name
+    The second argument will be the owner name
+    You must use the packages requests and sys
+    You are not allowed to import packages other than requests and sys
+    You don’t need to check arguments passed to the script (number or type)
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: ./100-github_commits.py <repository name> <owner name>")
+        sys.exit(1)
+
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+            repository_name, owner_name)
+
+    response = requests.get(url)
+    commits = response.json()
+    for i in range(10):
+        print("{}: {}".format(
+            commits[i].get("sha"),
+            commits[i].get("commit").get("author").get("name")))
