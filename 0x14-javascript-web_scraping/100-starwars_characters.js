145ca850461e159ea4a78c0244ef400c7515e2a3
root <sideeqkolade@gmail.com>
2023-05-24 03:05:45 +0100

    task 8

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-
+// Write a script that prints all characters of a Star Wars movie:
+// The first argument is the Movie ID - example: 3 = “Return of the Jedi”
+// Display one character name by line
+// You must use the Star wars API
+// You must use the module request
 const request = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/';
